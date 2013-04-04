a94c98ecdad027eec0ce046036a82fb69f37757c
Anatoly Ostroumov <ostroumov095@gmail.com>
2013-04-05 01:24:24 +0400

    vars

--- routes/main.js
+++ routes/main.js
@@ -1,16 +1,15 @@
-var path = require('path');
-var fs = require('fs');
+var path = require('path'),
+    fs = require('fs'),
+    childProcess = require('child_process'),
+    phantomjs = require('phantomjs');
+
+
 exports.index = function (request, response) {
     if (request.query['url']) {
         var url = request.query['url'];
-        var childProcess = require('child_process');
-        var phantomjs = require('phantomjs');
-
-//        console.log(phantomjs);
 
         var binPath = phantomjs.path;
         var imgName = 'temp/img' + (new Date().getTime()) + '.png';
-
 
         var childArgs = [
             path.join(__dirname, 'phantomjs-script.js'),
@@ -30,7 +29,7 @@ exports.index = function (request, response) {
         });
     } else {
         response.send(
-            '<html><head><title>WHERE DO WE GO TODAY?</title></head><body>' +
+            '<!doctype html><html lang="en"><head><title>WHERE DO WE GO TODAY?</title></head><body>' +
                 '<h1>Enter URL</h1>' +
                 '<form action="/" method="get"><input type="text" name="url" value="http://google.com?q=sample"><input type="submit" value="Go!"></form> ' +
                 '</body></html>');
