20a0ac682f0c34c0d7d261d400fba7ca6cdfc93d
Ostroumov Anatolij <ostroumov095@gmail.com>
2015-07-04 10:37:55 +0300

    updated socket.io version

--- public/javascripts/hunt.js
+++ public/javascripts/hunt.js
@@ -1,26 +1,21 @@
-var socket = io.connect('', {
-  'connect timeout': 1000
-});
-
-var model = {
-  'message': ko.observable(),
-  'siteshotUrl': ko.observable(),
-  'imageUrl': ko.observable(false),
-  'locked': ko.observable(false),
-}
-
-model.siteshotUrl.subscribe(function(newUrl){
-  if(newUrl){
+var
+  socket = io(),
+  model = {
+    'message': ko.observable(),
+    'siteshotUrl': ko.observable(),
+    'imageUrl': ko.observable(false),
+    'locked': ko.observable(false)
+  };
+model.siteshotUrl.subscribe(function (newUrl) {
+  if (newUrl) {
     socket.emit('siteshot', newUrl);
   } else {
     model.message('Enter URL...');
   }
 });
-
-ko.applyBindings(model);
-
-socket.on('siteshotResult', function(data){
+socket.on('siteshotResult', function (data) {
   model.locked(data.locked);
   model.message(data.message);
   model.imageUrl(data.imageUrl);
 });
+ko.applyBindings(model);
